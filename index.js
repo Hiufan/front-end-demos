887d30c6a3ca48fa6484b9321502ec33c48ac3bc
Hiufan <hiufan@qq.com>
2015-07-16 11:21:40 +0800

    update

--- index.js
+++ index.js
@@ -19,7 +19,6 @@
 
 
 var express = require('express');
-var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
@@ -30,8 +29,10 @@ var bodyParser = require('body-parser');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 5000));
+
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', __dirname + '/views');
 app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
@@ -40,7 +41,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
 
 // app.use('/', routes);
 // app.use('/users', users);
@@ -82,7 +83,6 @@ app.use(function(err, req, res, next) {
 });
 
 
-// module.exports = app;
-app.listen(3000,function(){
-  console.log('The server is running on port 3000.');
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
